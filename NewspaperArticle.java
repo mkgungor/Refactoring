e3fea3469aeb0dcca40a1f239111b595e6c572a6
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-02-28 21:48:40 +0300

    Refactored NewspaperArticle.java
    Using Extract Class and Extract Super Class pattern to create a has-a and is-a relationship between classes. Thereby, refactoring the code and decreasing code smells to make the code more modular.

--- NewspaperArticle.java
+++ NewspaperArticle.java
@@ -5,37 +5,18 @@
  */
 import java.util.*;
 
-public class NewspaperArticle {
-	private String title;
-	private String author;
+public class NewspaperArticle extends Document {
 	private int startPage;
 	private int endPage;
 	private Set<String> editors;
 	private String newspaper;
-	private Date date;
-	private String city;
-	private String state;
-	private String postCode;
 	
 	public NewspaperArticle(String title, String author, int startPage, int endPage, Set<String> editors, String newspaper, Date date, String city, String state, String postCode) {
-		this.title = title;
-		this.author = author;
+		super(title, author, date, city, state, postCode);
 		this.startPage = startPage;
 		this.endPage = endPage;
 		this.editors = editors;
 		this.newspaper = newspaper;
-		this.date = date;
-		this.city = city;
-		this.state = state;
-		this.postCode  = postCode;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public String getAuthor() {
-		return author;
 	}
 	
 	public int getStartPage() {
@@ -54,28 +35,8 @@ public class NewspaperArticle {
 		return newspaper;
 	}
 	
-	public Date getDate() {
-		return date;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-	
-	public String getState() {
-		return state;
-	}
-	
-	public String getPostCode() {
-		return postCode;
-	}
-	
 	public int numPages(){
 		return endPage - startPage + 1;
-	}
-	
-	public boolean sameAuthor(NewspaperArticle article){
-		return this.author.equals(article.author);
 	}
 	
 	public boolean sameNewspaper(NewspaperArticle article) {
@@ -94,14 +55,6 @@ public class NewspaperArticle {
 			}
 		}
 		return sameEditors;
-	}
-	
-	public int compareDates(NewspaperArticle article){
-		return this.date.compareTo(article.date);
-	}
-	
-	public int compareWithGeneralDate(Date date){
-		return this.date.compareTo(date);
 	}
 	
 }
